5644c4c1b57918eac12c89118d01bf782e20d256
cs8 <42040439+cscs8@users.noreply.github.com>
2020-01-04 19:13:22 +0900

    List5-20

--- chapter5/nuxt_app/store/index.js
+++ chapter5/nuxt_app/store/index.js
@@ -9,12 +9,22 @@ const createStore = () => {
             };
         },
         mutations: {
-            count: function (state, obj) {
-                state.message = obj.message;
-                state.counter += obj.add
+            count: function (state, n) {
+                state.counter += n;
+            },
+            say: function (state, message) {
+                state.message = message;
             },
             reset: function (state) {
                 state.counter = 0;
+                state.message = 'reset now...';
+            }
+        },
+        actions: {
+            doit: function (context) {
+                var n = Math.floor(Math.random() * 10);
+                context.commit('count', n);
+                context.commit('say', 'add ' + n + '!');
             }
         },
     }
